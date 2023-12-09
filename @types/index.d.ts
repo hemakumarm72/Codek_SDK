9bfa978e53a995b205f2078ca110955e174bf892
Hema Kumar <hemakumarm72@gmail.com>
2023-12-10 01:02:09 +0530

    coken testing

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,3 +1,21 @@
-export interface api {
-  projectId: string;
+export interface getProject {
+  type: 'opened' | 'closed';
 }
+
+export interface getProjectWallet {
+  type: 'opened' | 'closed';
+  walletAddress: string;
+}
+
+export interface newAPI {
+  ProjectId: string;
+}
+
+export declare type getProjectResponse = {
+  username: string;
+  title: string;
+  projectId: string;
+  type: 'opened' | 'closed';
+  gitHubRepo: string;
+  default_branch: string;
+};
--- index.ts
+++ index.ts
@@ -1,6 +1,35 @@
+import axios from 'axios';
+import { API_URL } from './config/env';
+import { getProject, getProjectResponse, getProjectWallet, newAPI } from './@types';
 
+export class CodeK {
+  private ProjectId: string;
+  private baseUrl: string = API_URL;
 
-const API_URL: string = 'https://githubevent.onrender.com/api/';
+  constructor(config: newAPI) {
+    this.ProjectId = config.ProjectId;
+    if (!this.ProjectId) throw new Error('project Id is null');
+  }
 
+  async getPullAll(input: getProject): Promise<getProjectResponse> {
+    try {
+      const result = await axios.get<getProjectResponse>(
+        `${this.baseUrl}/project/search/${this.ProjectId}?type=${input.type}`,
+      );
+      return result.data;
+    } catch (error) {
+      throw new Error('Failed to fetch user.');
+    }
+  }
 
-
+  async getPullByWallet(input: getProjectWallet): Promise<getProjectResponse> {
+    try {
+      const result = await axios.get<getProjectResponse>(
+        `${this.baseUrl}/project/search/${this.ProjectId}/${input.walletAddress}/wallet?type=${input.type}`,
+      );
+      return result.data;
+    } catch (error) {
+      throw new Error('Failed to fetch user.');
+    }
+  }
+}
